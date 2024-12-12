44608be902b105f2f71771f3c7d8b2788940aab5
Victoria Banjo <67071771+banjovic@users.noreply.github.com>
2024-12-12 15:14:28 +0100

    `feat: updated dashboard page with suspense and toast import`

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { Box, Typography } from "@mui/material";
-import React, { useEffect, useState } from "react";
+import React, { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import toast from "react-hot-toast";
+import { Box, Typography } from "@mui/material";
 
 import { User } from "@/types/types";
 import DashboardSkeleton from "@/components/DashboardSkeleton";
 import { useGetUserQuery } from "@/redux/dashboard/DashboardService";
-import toast from "react-hot-toast";
 
 const DashboardPage = () => {
   const router = useRouter();
@@ -61,74 +61,76 @@ const DashboardPage = () => {
   }
 
   return (
-    <Box width='100%' height='100vh'>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-          width: "100%",
-        }}
-        mx='auto'
-      >
+    <Suspense fallback={<DashboardSkeleton />}>
+      <Box width='100%' height='100vh'>
         <Box
-          display='flex'
-          flexDirection='column'
-          gap={2}
-          bgcolor='#F8F8F8'
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+            width: "100%",
+          }}
           mx='auto'
-          alignItems='center'
-          justifyContent='center'
-          width='70%'
-          borderRadius={3}
-          pb={2}
         >
           <Box
-            sx={{
-              width: "100%",
-              height: "100%",
-              background: `linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/login-bg.png') no-repeat center center`,
-              backgroundSize: "cover",
-              color: "white",
-              fontFamily: "var(--font-public-sans)",
-            }}
             display='flex'
             flexDirection='column'
-            justifyContent='center'
-            textAlign='center'
-            gap={4}
-            borderRadius={3}
-            p={4}
+            gap={2}
+            bgcolor='#F8F8F8'
             mx='auto'
+            alignItems='center'
+            justifyContent='center'
+            width='70%'
+            borderRadius={3}
+            pb={2}
           >
-            <Typography
-              variant='h3'
-              gutterBottom
-              fontWeight={700}
-              sx={{ fontFamily: "var(--font-public-sans)" }}
+            <Box
+              sx={{
+                width: "100%",
+                height: "100%",
+                background: `linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/login-bg.png') no-repeat center center`,
+                backgroundSize: "cover",
+                color: "white",
+                fontFamily: "var(--font-public-sans)",
+              }}
+              display='flex'
+              flexDirection='column'
+              justifyContent='center'
+              textAlign='center'
+              gap={4}
+              borderRadius={3}
+              p={4}
+              mx='auto'
             >
-              Welcome, John Doe
-            </Typography>
-            <Typography
-              variant='body2'
-              sx={{ fontFamily: "var(--font-public-sans)" }}
-            >
-              Super Admin
+              <Typography
+                variant='h3'
+                gutterBottom
+                fontWeight={700}
+                sx={{ fontFamily: "var(--font-public-sans)" }}
+              >
+                Welcome, John Doe
+              </Typography>
+              <Typography
+                variant='body2'
+                sx={{ fontFamily: "var(--font-public-sans)" }}
+              >
+                Super Admin
+              </Typography>
+            </Box>
+
+            <Typography variant='subtitle1' textAlign='center' mx='auto' mb={2}>
+              Korem ipsum dolor sit amet, consectetur adipiscing elit., Korem
+              ipsum dolor sit amet, consectetur adipiscing elit., Korem ipsum
+              dolor sit amet, consectetur adipiscing elit. Korem ipsum dolor sit
+              amet, consectetur adipiscing elit.,Korem ipsum dolor sit amet,
+              consectetur adipiscing elit.
             </Typography>
           </Box>
-
-          <Typography variant='subtitle1' textAlign='center' mx='auto' mb={2}>
-            Korem ipsum dolor sit amet, consectetur adipiscing elit., Korem
-            ipsum dolor sit amet, consectetur adipiscing elit., Korem ipsum
-            dolor sit amet, consectetur adipiscing elit. Korem ipsum dolor sit
-            amet, consectetur adipiscing elit.,Korem ipsum dolor sit amet,
-            consectetur adipiscing elit.
-          </Typography>
         </Box>
       </Box>
-    </Box>
+    </Suspense>
   );
 };
 
