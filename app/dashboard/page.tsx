083e73ab2be1487caac9a42c5465672c91746db5
Victoria Banjo <67071771+banjovic@users.noreply.github.com>
2024-12-12 15:55:12 +0100

    `Refactor DashboardPage component: rename to DashboardPageContent, update JSX structure and styles`

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { useSearchParams, useRouter } from "next/navigation";
 import DashboardSkeleton from "@/components/DashboardSkeleton";
 import { useGetUserQuery } from "@/redux/dashboard/DashboardService";
 
-const DashboardPage = () => {
+const DashboardPageContent = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const user_id = searchParams.get("user_id");
@@ -51,77 +51,82 @@ const DashboardPage = () => {
   }
 
   return (
-    <Suspense fallback={<DashboardSkeleton />}>
-      <Box width='100%' height='100vh'>
+    <Box width='100%' height='100vh'>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          width: "100%",
+        }}
+        mx='auto'
+      >
         <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            height: "100%",
-            width: "100%",
-          }}
+          display='flex'
+          flexDirection='column'
+          gap={2}
+          bgcolor='#F8F8F8'
           mx='auto'
+          alignItems='center'
+          justifyContent='center'
+          width='70%'
+          borderRadius={3}
+          pb={2}
         >
           <Box
+            sx={{
+              width: "100%",
+              height: "100%",
+              background: `linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/login-bg.png') no-repeat center center`,
+              backgroundSize: "cover",
+              color: "white",
+              fontFamily: "var(--font-public-sans)",
+            }}
             display='flex'
             flexDirection='column'
-            gap={2}
-            bgcolor='#F8F8F8'
-            mx='auto'
-            alignItems='center'
             justifyContent='center'
-            width='70%'
+            textAlign='center'
+            gap={4}
             borderRadius={3}
-            pb={2}
+            p={4}
+            mx='auto'
           >
-            <Box
-              sx={{
-                width: "100%",
-                height: "100%",
-                background: `linear-gradient(to bottom, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.3)), url('/login-bg.png') no-repeat center center`,
-                backgroundSize: "cover",
-                color: "white",
-                fontFamily: "var(--font-public-sans)",
-              }}
-              display='flex'
-              flexDirection='column'
-              justifyContent='center'
-              textAlign='center'
-              gap={4}
-              borderRadius={3}
-              p={4}
-              mx='auto'
+            <Typography
+              variant='h3'
+              gutterBottom
+              fontWeight={700}
+              sx={{ fontFamily: "var(--font-public-sans)" }}
             >
-              <Typography
-                variant='h3'
-                gutterBottom
-                fontWeight={700}
-                sx={{ fontFamily: "var(--font-public-sans)" }}
-              >
-                Welcome, John Doe
-              </Typography>
-              <Typography
-                variant='body2'
-                sx={{ fontFamily: "var(--font-public-sans)" }}
-              >
-                Super Admin
-              </Typography>
-            </Box>
-
-            <Typography variant='subtitle1' textAlign='center' mx='auto' mb={2}>
-              Korem ipsum dolor sit amet, consectetur adipiscing elit., Korem
-              ipsum dolor sit amet, consectetur adipiscing elit., Korem ipsum
-              dolor sit amet, consectetur adipiscing elit. Korem ipsum dolor sit
-              amet, consectetur adipiscing elit.,Korem ipsum dolor sit amet,
-              consectetur adipiscing elit.
+              Welcome, John Doe
+            </Typography>
+            <Typography
+              variant='body2'
+              sx={{ fontFamily: "var(--font-public-sans)" }}
+            >
+              Super Admin
             </Typography>
           </Box>
+
+          <Typography variant='subtitle1' textAlign='center' mx='auto' mb={2}>
+            Korem ipsum dolor sit amet, consectetur adipiscing elit., Korem
+            ipsum dolor sit amet, consectetur adipiscing elit., Korem ipsum
+            dolor sit amet, consectetur adipiscing elit. Korem ipsum dolor sit
+            amet, consectetur adipiscing elit.,Korem ipsum dolor sit amet,
+            consectetur adipiscing elit.
+          </Typography>
         </Box>
       </Box>
-    </Suspense>
+    </Box>
   );
 };
 
-export default DashboardPage;
+// export default DashboardPage;
+export default function DashboardPage() {
+  return (
+    <Suspense fallback={<DashboardSkeleton />}>
+      <DashboardPageContent />
+    </Suspense>
+  );
+}
